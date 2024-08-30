cadc7ed894a07be1ecbf4f117b06487993bcfa93
이윤정 <102017351+leeyunjeong010422@users.noreply.github.com>
2024-08-30 21:07:14 +0900

    Feat: 코루틴 FPS 중간마무리

--- HomeWork_Pool_Ray/Assets/Scripts/Coroution_FPS/Co_Target.cs
+++ HomeWork_Pool_Ray/Assets/Scripts/Coroution_FPS/Co_Target.cs
@@ -17,6 +17,10 @@ public class Co_Target : MonoBehaviour
         }
     }
 
+    //코루틴으로 죽으면 3초 뒤에 살아나도록 했는데 잘 안 되어서
+    //활성화 비활성화를 이용해서 죽으면 비활성화 되었다가 3초 뒤에 다시 활성화되게 했는데
+    //이것도 비활성화 되었을 때 몬스터 프리팹을 찾지 못해서 활성화되지 못합니다...
+    //어떻게 수정을 해야할지 모르겠어요 ㅠㅠ...
     private void Die()
     {
         //makeMonster.MakeMonster();
